4077c0979b3b194f32e8cb50fc20827a3ab9b456
MahaSiva1 <mahasivas4@gmail.com>
2024-11-18 19:07:47 -0600

    worked on first page of website
    Co-authored-by: Umer Khan <umerkhan5110@gmail.com>

--- my-app/src/UploadMesh.js
+++ my-app/src/UploadMesh.js
@@ -17,12 +17,47 @@ function UploadMesh() {
   return (
     <div className="">
      <div className="flex">
-      <div className="w-1/3"> <input type="file" id="file" name="file" multiple /> </div>
-      <div className="w-2/3 py-10"> <div className="bg-gray-600 w-[90%]"> hhe</div></div>
-      
-     </div>
+      <div className="w-1/3"> 
+        <input type="file" id="file" name="file" multiple /> 
+      </div>
+      <div className="w-2/3 py-10"> 
+        <div className="bg-gray-600 w-[90%]"> preview screen</div>
+      </div>
+      <p> Choose which bone in the mesh relates to which human joint </p>
+      <div></div>
+      <table>
+      <thead>
+        <tr>
+          <th>Bone</th>
+          <th>Human Joint</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <select>
+              <option value="Option">Sample Bone 1</option>
+              <option value="opt2">Sample Bone 2</option>
+              <option value ="opt3">Sample Bone 3</option>
+            </select>
+          </td>
+          <td>
+            <select>
+              <option value="opt1">Sample Joint</option>
+              <option value="opt2">Sample joint2</option>
+              <option value="opt3">Sample joint3</option>
+            </select>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button>
+      <a type="button" href="nextPage.html">Next Page</a>
+    </button>
     </div>
-  );
+  </div>
+);
+
 }
 
 export default UploadMesh;
