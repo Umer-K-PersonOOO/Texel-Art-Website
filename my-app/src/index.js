46f1f139ffefa3991720e7d3e3702037c5e9b16f
anna-spark <66567710+anna-spark@users.noreply.github.com>
2025-04-24 18:46:52 -0500

    fix

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import UploadMesh from './UploadMesh';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
